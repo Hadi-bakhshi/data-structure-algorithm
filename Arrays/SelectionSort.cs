1413e76239439e5fae3b5ed529d0d7d183df1953
Hadi Bakhshi <hadibakhshi277@gmail.com>
2025-03-27 15:33:42 +0330

    Add project files.

---
+++ Arrays/SelectionSort.cs
@@ -0,0 +1,26 @@
+﻿namespace dsa.Arrays;
+
+/// <summary>
+/// the worst and the average time complexity is O(n2)
+/// </summary>
+internal class SelectionSort
+{
+    public static void Sort(int[] numbers)
+    {
+        for (int i = 0; i < numbers.Length - 1; i++)
+        {
+            int minIndex = i;
+            int minValue = numbers[i];
+
+            for (int j = i + 1; j < numbers.Length; j++)
+            {
+                if (numbers[j] < minValue)
+                {
+                    minIndex = j;
+                    minValue = numbers[j];
+                }
+            }
+            (numbers[i], numbers[minIndex]) = (numbers[minIndex], numbers[i]);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using dsa.Arrays;
+
+int[] numbers = [-11, 12, -42, 0, 1, 90, 68, 6, -9];
+
+SelectionSort.Sort(numbers);
+
+Console.WriteLine(string.Join(" | ", numbers));
