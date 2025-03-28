1ab0af92a76c809dc5679ccf4b41d75dd5de01e0
Hadi Bakhshi <hadibakhshi277@gmail.com>
2025-03-28 23:58:18 +0330

    feat: add insertion sort

---
+++ Arrays/InsertionSort.cs
@@ -0,0 +1,20 @@
+﻿namespace dsa.Arrays;
+
+/// <summary>
+/// the worst and average time complexity is O(n2)
+/// </summary>
+internal class InsertionSort
+{
+    public static void Sort(int[] numbers)
+    {
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            int j = i;
+            while (j > 0 && numbers[j] < numbers[j - 1])
+            {
+                (numbers[j], numbers[j - 1]) = (numbers[j - 1], numbers[j]);
+                j--;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,6 @@
 
 int[] numbers = [-11, 12, -42, 0, 1, 90, 68, 6, -9];
 
-SelectionSort.Sort(numbers);
-
+// SelectionSort.Sort(numbers);
+InsertionSort.Sort(numbers);
 Console.WriteLine(string.Join(" | ", numbers));
